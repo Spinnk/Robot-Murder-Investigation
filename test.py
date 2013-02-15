8d302ff7d24965c74f93c75a705061637687a445
T.J. Callahan <callat2@gmail.com>
2013-02-15 17:43:59 -0500

    messing with python

--- test.py
+++ test.py
@@ -3,9 +3,15 @@ THIS IS A TEST
 SUBLIME TEXT IS COOL IF YOU DON'T HAVE IT IT IS PRETTY MUCH THE BEST
 """
 
+#and now i'm just messing around in python
+import os
 import sys
 
 def main():
+    dirs=os.listdir("c:/users/Tim/Documents/Github")
+    print dirs
+    print "hello"
+    print "hello"	
 
-	
-
+if __name__=="__main__":
+        main()
