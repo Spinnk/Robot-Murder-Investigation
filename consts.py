c8b443719bf696b0ebf54d4363c90747c958388e
Kate <ktspinney@gmail.com>
2013-03-19 16:36:51 -0400

    Organization/Consts modification

--- consts.py
+++ consts.py
@@ -18,17 +18,23 @@ MAP_DEFAULT = ""                            # default map configuration file
 MAP_WIDTH = 1024                            # tiles
 MAP_HEIGHT = 1024                           # tiles
 
+NUM_TILE_TYPES = 16
+
 TILE_SHEET = ""                             # sheet of non-animated images
 TILE_WIDTH = 80                             # pixels
 TILE_HEIGHT = 80                            # pixels
 SHOW_TILES_W = SCREEN_WIDTH / TILE_WIDTH    # number of tiles across that are shown at any one time
 SHOW_TILES_H = SCREEN_HEIGHT / TILE_HEIGHT  # number of tiles down that are shown at any one time
 
+ART_LOCATION = os.path.join( CWD, "art" )
+
+BACKGROUND_IMAGE = os.path.join( ART_LOCATION, "background 1.png" )
+
 SPRITE_SHEETS = ["", ""]                    # strings of file names. probably going to create separate variables rather than use list
 SPRITE_WIDTH = 80                           # pixels
 SPRITE_HEIGHT = 80                          # pixels
 
-CHARACTER_SPRITE_SHEET = os.path.join(CWD, "robot.png")
+CHARACTER_SPRITE_SHEET = os.path.join( ART_LOCATION, "robot.png")
 CHARACTER_WIDTH = 80                        # pixels
 CHARACTER_HEIGHT = 188                      # pixels
 
