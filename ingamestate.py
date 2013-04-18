fa233afe0a8eafa5b6337248e05528c4b72772e2
Kate <ktspinney@gmail.com>
2013-04-18 16:37:04 -0400

    Fixed User Update Error

--- ingamestate.py
+++ ingamestate.py
@@ -85,8 +85,8 @@ class InGameState (GameState):
             # Attempt to talk to a nearby NPC if "enter" key is pressed
             elif event.key == self.keybindings[KB_ENTER]:
                 self.attempt_dialogue()
-
-        self.user.update(event)
+            else:
+                self.user.update(event)
         return IN_GAME_STATE
 
     ## ---[ display ]----------------------------------------------------------
@@ -132,10 +132,7 @@ class InGameState (GameState):
     def removeitem(self):
         item = self.ship.removeitem([self.user.getx(), self.user.gety() + 1])
         if item != None:
-            if item == 1:
-                self.inventory.additem(1, 1)
-            else:
-                self.inventory.additem(item)
+            self.inventory.additem(item)
 
     # set all floor items
     def setitemsonfloor(self, itemsonfloor):
@@ -199,5 +196,5 @@ class InGameState (GameState):
 
 
 
-
+            
 
