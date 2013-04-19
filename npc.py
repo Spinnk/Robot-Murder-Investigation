0bd4cf1552bef973e7bdf332893185eb11ced71b
Geo Kersey <geokersey@gmail.com>
2013-04-19 17:15:18 -0400

    Forgot something important

--- npc.py
+++ npc.py
@@ -46,7 +46,7 @@ class NPC:
         self.font = pygame.font.Font(NPC_FONT_DIR, NPC_FONT_SIZE)
 
         self.dialogue = []      # dialogue is a list, where each element is an array of
-                                # [mission, preconditions (including response flags), postconditions, dialogue, [response flag, response dialogue], [response flag, response dialogue], ...]
+                                # [mission, preconditions (including response flags), postconditions, dialogue, (response flag, response dialogue), (response flag, response dialogue), ...]
 
     # Accessors and Modifiers
 
@@ -91,7 +91,7 @@ class NPC:
         self.sayindex = 0
 
     # read in dialogue
-    def readdialogue(self, dialogue):
+    def readdialogue(self, self.dialogue):
         f = open("dialogue%s.txt" %self.ID, 'r')    #open dialogue#.txt, where # is the ID of this NPC
         line = f.readline()
         while line != '':
@@ -105,7 +105,7 @@ class NPC:
                     del splitline[5:7]
                     splitline.insert(i, response)
                     
-            dialogue.append(splitline)
+            self.dialogue.append(splitline)
             line = f.readline()
 
     # load from save string
